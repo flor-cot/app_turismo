bad8f3c30d947638ba7eb32f3e4c0f0039551749
rominapamela <rominapamelavelazquez@yahoo.com.ar>
2022-10-10 14:17:39 -0300

    ruta y template para destinos

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
-
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 def index(request):
     return HttpResponse('Este es el index.')
+
+def destinos(request,provincia=""):
+    return render(request,'app/destinos.html',{'provincia': provincia})
