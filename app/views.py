d78d29a2f1f5623a7ecf1a95024ed90f58aa72c2
Maxi Torres <torres_maximiliano@hotmail.com>
2022-10-10 18:45:25 -0300

    Resolución de conflictos con las ramas rama_maxi y rama_romy2

--- app/views.py
+++ app/views.py
@@ -2,11 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-def index(request):
-    return HttpResponse('Este es el index.')
 
-def destinos(request,provincia=""):
-    return render(request,'app/destinos.html',{'provincia': provincia})
+
+def index(request):
     contexto = {
         'hoteles' : (
             {'nombre': 'Dormís Acá',
@@ -33,6 +31,9 @@ def destinos(request,provincia=""):
     }
     return render(request, 'app/index.html', contexto)
 
+
+def destinos(request,provincia=""):
+    return render(request,'app/destinos.html',{'provincia': provincia})
 
 def about(request):
     data = {'hoteles':('hola','chau')}
