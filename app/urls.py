23e174b26f49622430d22df2a59bab8432da6049
Florencia Lucia <you@example.com>
2022-11-14 12:00:12 -0300

    se usa el objeto User y se realizan los forms de registro y login. Se mejora CSS del index

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name ='index'),
-    path('login',views.login, name='login'),
+    path('login',views.iniciar_sesion, name='login'),
+    path('logout',views.cerrar_sesion, name='logout'),
     path('registro',views.registro, name='registro'),
     path('destinos/<str:provincia>',views.destinos,name="destinos"),
     path('hotel/<int:id_hotel>', views.hotel_detalle, name="hotel_detalle"),
--- app/views.py
+++ app/views.py
@@ -8,38 +8,81 @@ from app.models import Provincia, Hotel, Comentario
 
 from django.template import loader
 
-from app.forms import RegistroForm, LoginForm
+from app.forms import RegistroForm
 
 from django.contrib import messages
 
-def login(request):
-    
-     if(request.method == 'POST'):
-        
-        login_form = LoginForm(request.POST)
-        if(login_form.is_valid()):
-            return HttpResponseRedirect(reverse('index'))
-        else:
-            messages.warning(request,'Por favor revisa los errores')
-     else:
-        login_form = LoginForm()
+from django.contrib.auth.forms import UserCreationForm
 
-     return render(request,'app/login.html',
-                {'login_form':login_form})
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
+
+
+def iniciar_sesion(request):
+    
+    #  if(request.method == 'POST'):
+        
+    #     login_form = LoginForm(request.POST)
+    #     if(login_form.is_valid()):
+    #         return HttpResponseRedirect(reverse('index'))
+    #     else:
+    #         messages.warning(request,'Por favor revisa los errores')
+    #  else:
+    #     login_form = LoginForm()
+
+    #  return render(request,'app/login.html',
+    #             {'login_form':login_form})
+    if request.user.is_authenticated:
+        return redirect('index')    
+    else:
+        if(request.method == 'POST'):
+            usuario = request.POST.get('usuario')
+            password =request.POST.get('password')  
+
+            #busca al usuario en la BDD
+            user = authenticate(request, username=usuario, password=password)   
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'Usuario O contraseña incorrecto')   
+
+        return render(request,'app/login.html')
+
+def cerrar_sesion(request):
+	logout(request)
+	return redirect('login')
+
 
 def registro(request):
-     if(request.method == 'POST'):
-        registro_form = RegistroForm(request.POST)
-        if(registro_form.is_valid()):
-            messages.success(request,'Muchas gracias por registrarte.')
-        else:
-            messages.warning(request,'Por favor revisa los errores')
-     else:
-        registro_form = RegistroForm()
+    #  if(request.method == 'POST'):
+    #     registro_form = RegistroForm(request.POST)
+    #     if(registro_form.is_valid()):
+    #         messages.success(request,'Muchas gracias por registrarte.')
+    #     else:
+    #         messages.warning(request,'Por favor revisa los errores')
+    #  else:
+    #     registro_form = RegistroForm()
+        if request.user.is_authenticated:
+            return redirect('index')
+        else:  
+            registro_form = RegistroForm()
 
-     return render(request,'app/registro.html',
-                {'registro_form':registro_form})
+            if request.method == 'POST':
+                registro_form = RegistroForm(request.POST)
+                if registro_form.is_valid():
+                    registro_form.save()
+                    usuario = registro_form.cleaned_data.get('username')
+                    messages.success(request,'La cuenta de' + ' ' + usuario + ' ' + 'fue creada con éxito.')
+                    return redirect('login')
 
+            return render(request,'app/registro.html',
+                        {'registro_form':registro_form})
+
+# @login_required(login_url='login')     #Decorador para restringir acceso a suarios NO logueados
 def index(request):
     provincias = Provincia.objects.all()
     hoteles = Hotel.objects.order_by('-puntuacion').all()
