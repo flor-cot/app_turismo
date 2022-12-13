17fe2e39dc644ff025802f1bb8e0341aea703c02
rominapamela <rominapamelavelazquez@yahoo.com.ar>
2022-12-13 00:05:32 -0300

    creacion de edicion de perfil

--- app/urls.py
+++ app/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
     path('',views.index, name ='index'),
     path('login/',views.iniciar_sesion, name='login'),
     path('logout/',views.cerrar_sesion, name='logout'),
+    path('miperfil/',views.mi_perfil, name='miperfil'),
     path('registro/',views.registro, name='registro'),
     path('destinos/<int:id_provincia>', views.destinos, name="destinos"),
     path('detalle/<int:id_destino>', views.undestino, name="undestino"),
     path('hotel/<int:id_hotel>', views.hotel_detalle, name="hotel_detalle"),
-    path('about/', views.about, name="about")
+    path('about/', views.about, name="about"),
+    path('password/', views.cambiar_password, name="cambiar_password")
 ]
--- app/views.py
+++ app/views.py
@@ -3,17 +3,15 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from app.models import Provincia, Hotel, Comentario, Atraccion, Ciudad
 from django.template import loader
-
-from app.forms import RegistroForm
+from app.forms import RegistroForm, EditProfileForm
 
 from django.contrib import messages
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from django.contrib.auth import authenticate, login, logout
 
 from django.contrib.auth.decorators import login_required
-
 
 
 def iniciar_sesion(request):
@@ -53,6 +51,25 @@ def cerrar_sesion(request):
 	logout(request)
 	return redirect('login')
 
+@login_required
+def mi_perfil(request):
+    if request.method == 'POST':
+        edit_form = EditProfileForm(request.POST, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            messages.success(request,'Los datos fueron modificados con éxito.')
+            return redirect('miperfil')
+        else:
+            messages.warning(request,'Los datos no pudieron ser modificados.')
+            return redirect('miperfil')
+    else:
+        edit_form = EditProfileForm(instance=request.user)
+        args = {'form': edit_form}
+        return render(request,'app/miperfil.html', args)
+
+@login_required
+def cambiar_password(request):
+    return render(request,'app/nueva_password.html')
 
 def registro(request):
     #  if(request.method == 'POST'):
